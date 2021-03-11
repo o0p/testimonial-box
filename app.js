b3d5b1d233f77ce52f3629a8849713297d89667c
Russell <russelldev@protonmail.com>
2021-03-11 23:36:22 +0000

    Add userImage selector

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 const testimonialsContainer = document.querySelector('.testimonial-container');
 const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
