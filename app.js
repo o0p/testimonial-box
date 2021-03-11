5420a3b222c1ad79b3373c473b3b8099cb84af26
Russell <russelldev@protonmail.com>
2021-03-11 23:37:05 +0000

    Add role selector

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const testimonialsContainer = document.querySelector('.testimonial-container');
 const testimonial = document.querySelector('.testimonial');
 const userImage = document.querySelector('.user-image');
 const username = document.querySelector('.username');
+const role = document.querySelector('.role');
